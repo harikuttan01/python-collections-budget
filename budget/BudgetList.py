c990140ddbcc70fbd411b6e345ce5ccfc76e8515
harikuttan01 <harikuttan01@users.noreply.github.com>
2022-05-30 21:01:24 +0530

    Completed module

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -10,7 +10,7 @@ class BudgetList:
         self.overages = []
 
     def append(self,item):
-        if self.sum_expenses+item <= self.budget:
+        if self.sum_expenses+item < self.budget:
             self.expenses.append(item)
             self.sum_expenses+=item
         else:
